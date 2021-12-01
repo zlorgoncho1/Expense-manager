54310d7a9d7afbe395121e104573c892eb928359
zlorgoncho1 <papamatardiop3@gmail.com>
2021-12-01 17:56:40 +0000

    First Commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
+
+
+const App = () => {
+  const [expenses, setExpenses] = useState([]);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
+    </div>
+  );
+};
+
+export default App;
